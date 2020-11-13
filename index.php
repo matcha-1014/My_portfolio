6e472b8f2f44079255079a230edb888a0d555428
matcha1014 <shadow1014green@gmail.com>
2020-11-13 17:38:03 +0900

    index.php、stylesheet.cssを作成

---
+++ index.php
@@ -0,0 +1,32 @@
+﻿<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>MyPage</title>
+    <link rel="stylesheet" href="stylesheet.css">
+  </head>
+
+  <body>
+    <header>
+      <div class="header-logo">
+        井上昌彦のホームページ
+      </div>
+      <div class="header-list">
+        <ul>
+          <li>自己紹介</li>
+          <li>キャリア</li>
+        </ul>
+      </div>
+    </header>
+
+    <div class="main">
+      <div class="container">
+        <div class="introduction">
+          <h1>こんにちは!</h1>
+          <h2>井上昌彦のホームページへようこそ！</h2>
+        </div>
+        <img src="../img/bungee.jpg" width="768" height="512">
+      </div>
+    </div>
+  </body>
+</html>
