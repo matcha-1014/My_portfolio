fc67a5d55c217d644d22f289dacdb0c89869f23a
matcha1014 <shadow1014green@gmail.com>
2020-11-14 17:46:20 +0900

    自己紹介、キャリアの内容、視認性を改善。

--- index.php
+++ index.php
@@ -23,15 +23,19 @@
 
     <div class="intro-wrapper">
       <div class="container">
-        <h2>自己紹介</h2>
+        <h2 class="section-title">自己紹介</h2>
         <h3>名前: matcha</h3>
         <h3>年齢: 24</h3>
+        <h3>一言: Webエンジニアを目指しています。フロント、サーバー両方に携われるようになりたいです。</h3>
       </div>
     </div>
 
     <div class="carrer-wrapper">
       <div class="container">
-        <h2>キャリア</h2>
+        <h2 class="section-title">キャリア</h2>
+        <h3>2015年: 法政大学情報科学部コンピュータ科学科 入学</h3>
+        <h3>2019年: 法政大学情報科学部コンピュータ科学科 卒業</h3>
+        <h3>2020年: ProgateでHTML & CSS、JavaScript、Gitを学習。現在レベル65。</h3>
       </div>
     </div>
   </body>
